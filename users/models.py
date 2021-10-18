704d4c7e4cc76058e52a100711a1c8368a5c9029
Andrius Makutas <andrius.makutas@gmail.com>
2021-10-18 21:15:13 +0300

    added user app additional UserProfile model to hold info about user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# TODO - custom UserProfile model with description, avatar, uploaded recipe count?
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    user_description = models.CharField(max_length=200, null=True)
+    user_avatar = models.ImageField(null=True, blank=True)
+    user_uploaded_recipes = models.IntegerField() # Increment by 1 on upload
